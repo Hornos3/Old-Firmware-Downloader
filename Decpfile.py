46a614169590348082d6f1626cf9ca7648849a63
Hornos Lee <81894076+Hornos3@users.noreply.github.com>
2021-07-21 20:13:23 +0800

    Extract and sort files from zip or rar files

--- Decpfile.py
+++ Decpfile.py
@@ -1,13 +1,39 @@
 # coding = utf-8
-import zipfile, os, re, shutil
-def ext(file, dir):
-    file_to_dezip = zipfile.ZipFile(file, 'r')
-    print('Start extracting file %s' % file)
-    for f in file_to_dezip.namelist():
-        file_to_dezip.extract(f, './Firmwares/Decompressed/' + dir[2:])
-        print('File %s extracted.' % f)
-        if re.match(".+\\.zip$", f):
-            ext('./Firmwares/Decompressed/' + dir[2:] + '/' + f, './Firmwares/Decompressed/' + dir[2:] + '/' + f)
+import zipfile, os, re, shutil, rarfile
+def ext(file, dir, suffix):
+    if suffix == 'zip':
+        file_to_dezip = zipfile.ZipFile(file, 'r')
+        print('Start extracting file %s' % file)
+        for f in file_to_dezip.namelist():
+            file_to_dezip.extract(f, './Firmwares/Decompressed/' + dir[2:])
+            print('File %s extracted.' % f)
+            if re.match(".+\\.zip$", f):
+                ext('./Firmwares/Decompressed/' + dir[2:] + '/' + f, './Firmwares/Decompressed/' + dir[2:] + '/' + f,
+                    'zip')
+    elif suffix == 'rar':
+        israr = True
+        try:
+            file_to_derar = rarfile.RarFile(file, 'r')
+            print('Start extracting file %s' % file)
+        except rarfile.NotRarFile:
+            israr = False
+            try:
+                ext(file, dir, 'zip')
+            except zipfile.error:
+                print('This file is neither a zip file nor a rar file!')
+        finally:
+            if israr:
+                for f in file_to_derar.namelist():
+                    try:
+                        file_to_derar.extract(f, './Firmwares/Decompressed/' + dir[2:])
+                        print('File %s extracted.' % f)
+                    except rarfile.BadRarFile:
+                        print('Uh oh, This file was damaged!')
+                    finally:
+                        if re.match(".+\\.rar$", f):
+                            ext('./Firmwares/Decompressed/' + dir[2:] + '/' + f,
+                                './Firmwares/Decompressed/' + dir[2:] + '/' + f,
+                                'rar')
 
 def copyfile(dirname, folder):
     for dirname, folders, filenames in list(os.walk(dirname + '/' + folder)):
@@ -35,11 +61,11 @@ def order(filenames):
                 for folder in folders:
                     copyfile(dirname, folder)
 
-def decompress(filenames):
+def decompress(filenames, suffix):
     if not os.path.isdir('./Firmwares/Decompressed'):
         os.mkdir('./Firmwares/Decompressed')
     for file in filenames:
-        ext(file, file)
+        ext(file, file, suffix)
     IsOrder = input('Do you want to order those decompressed file into different suffixes? Y / N ')
     if IsOrder == 'y' or IsOrder == 'Y':
         filenames.append('./Firmwares/Decompressed')
